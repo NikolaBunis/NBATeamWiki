cbf5efa95ae6727254af7c238339d8566bd430bb
[NikolaBunis] <[bunisbg@gmail.com]>
2018-08-08 17:19:55 +0300

    The code for setting up the drawer and title bar is now in a method which should be inherited by all activities from their base abstract class.

--- AndroidStudioProjects/NBATeamWiki/app/src/main/java/com/example/nikola/nbateamwiki/views/views/MainTeamPage/MainTeamPageActivity.java
+++ AndroidStudioProjects/NBATeamWiki/app/src/main/java/com/example/nikola/nbateamwiki/views/views/MainTeamPage/MainTeamPageActivity.java
@@ -1,7 +1,6 @@
 package com.example.nikola.nbateamwiki.views.views.MainTeamPage;
 
 import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 import com.example.nikola.nbateamwiki.R;
@@ -17,20 +16,16 @@ private Fragment mToolbarFragment;
         setContentView(R.layout.activity_main_team_page);
 
         mToolbarFragment = DrawerAndTitleFragment.newInstance();
-        FragmentTransaction transaction = getFragmentManager()
-                .beginTransaction();
-
-        transaction.replace(R.id.drawer_toolbar, mToolbarFragment).commit();
-
-
-        //mToolbarFragment.getFragmentManager().beginTransaction().replace(R.id.drawer_toolbar, mToolbarFragment).commit();
-
+        setupDrawer();
 
 
     }
 
     @Override
     public void setupDrawer() {
+
+getFragmentManager().beginTransaction().replace(R.id.drawer_toolbar, mToolbarFragment).commit();
+
 
     }
 }
